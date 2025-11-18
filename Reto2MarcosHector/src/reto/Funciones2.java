b9dcbce1f1b4f69e9e38fd755ec87f3ba8833571
tegocalderonn <1DAM@A11-PC19>
2025-11-18 11:56:02 +0100

    Creacion de funciones para pasar a:
    	1.- Celsius a Farenheit || Farenheir -> Celsius
    	2.- Litros a Galones || Galones -> Litros

--- Reto2MarcosHector/src/reto/Funciones2.java
+++ Reto2MarcosHector/src/reto/Funciones2.java
@@ -1,5 +1,26 @@
+// Daniel Ortego
+
 package reto;
 
 public class Funciones2 {
+	
+	
+public static double conversionAFarenheit(double c) {
+	return ((c*9/5)+32);
+}
+public static double conversorACensius(double f) {
+	return ((f-32)*5/9);
+}
+public static double conversorAGalones(double l) {
+	return (l/3.785);
+}
+public static double conversorALitros(double g) {
+	return (g*3.785);
+}
+
+
+
+
+
 
 }
