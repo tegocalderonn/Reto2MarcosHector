4e92dc907c90321b42cf4f3878672427a28b0f9d
tegocalderonn <1DAM@A11-PC19>
2025-11-18 12:23:11 +0100

    Do - while y switch

--- Reto2MarcosHector/src/reto/Funciones2.java
+++ Reto2MarcosHector/src/reto/Funciones2.java
@@ -1,6 +1,8 @@
-// Daniel Ortego
+// Daniel
 
 package reto;
+
+import java.util.Scanner;
 
 public class Funciones2 {
 
@@ -18,6 +20,33 @@ public class Funciones2 {
 
 	public static double conversorALitros(double g) {
 		return (g * 3.785);
+	}
+	public static void menuVolumen(Scanner sc) {
+		int f = 0;
+		do {
+			System.out.println("=== Selecciona el tipo de conversion ===");
+			System.out.println("1. De celsius a farenheit");
+			System.out.println("2. De farenheir a celsius");
+			System.out.println("0. Salir ");
+			String entrada = sc.nextLine();
+			f = Integer.parseInt(entrada);
+			switch (f) {
+			case 1:
+				System.out.println("Introduce los grados a convertir a farenheit");
+				String e2 = sc.nextLine();
+				double cant = Double.parseDouble(e2);				
+				System.out.println(conversionAFarenheit(cant) + "ºF");
+				break;
+			case 2:
+				System.out.println("Introduce los grados farenheit a convertir a celsius");
+				String e3 = sc.nextLine();
+				double cant2 = Double.parseDouble(e3);
+				System.out.println(conversionAFarenheit(cant2) + "C");
+				break;
+			default: System.out.println("Valor introducido no valido.");
+				break;
+			}
+		} while (f != 0);
 	}
 
 }
